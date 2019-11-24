c67f91ccd6a9721af80e182a86e68c09fba30977
obedouet <baddy@free.fr>
2019-11-24 10:49:52 +0100

    spooler for diris in stdin mode

---
+++ diris_spool.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SPOOL_DIR=/run/diris
+MODBUS_READ=/home/pi/Socomec/modbus_read
+
+cd ${SPOOL_DIR}
+while [ 1 ]
+do
+    ls | $MODBUS_READ | while read RESULT
+    do
+        REGISTER=`echo $RESULT | awk -F: '{print $1}'`
+        VALUE=`echo $RESULT | awk -F: '{print $2}'`
+        [ -n "$VALUE" ] && echo $VALUE > $REGISTER
+    done
+
+done
