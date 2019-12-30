46c8921192e4fe11096014148d032cfb89e02404
obedouet <baddy@free.fr>
2019-12-30 22:48:58 +0100

    changes: convert str to int

--- api/v1/voltage.py
+++ api/v1/voltage.py
@@ -7,7 +7,7 @@ class phase1_voltage(Resource):
     def get(self):
         voltage = read_counter(50520)
         if voltage != None:
-            return voltage
+            return int(voltage)
         else:
             return {'error':'no value'}, 500
 
@@ -15,7 +15,7 @@ class phase2_voltage(Resource):
     def get(self):
         voltage = read_counter(50522)
         if voltage != None:
-            return voltage
+            return int(voltage)
         else:
             return {'error':'no value'}, 500
 
@@ -23,6 +23,6 @@ class phase3_voltage(Resource):
     def get(self):
         voltage = read_counter(50524)
         if voltage != None:
-            return voltage
+            return int(voltage)
         else:
             return {'error':'no value'}, 500
