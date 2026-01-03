eb5df2b5f5d2f61e3bd9fac40c78c3c7400c0b93
obedouet <obedouet@users.noreply.github.com>
2026-01-03 15:29:29 +0100

    add: total kwh

--- api/v1/power.py
+++ api/v1/power.py
@@ -6,127 +6,117 @@ from v1.common import read_counter
 class total_real_power(Resource):
     def get(self):
         power = read_counter(790)
-        if power != None:
+        if power is not None:
             # Real Power must be converted
             if int(power) > 32768:
                 return int(power)-65535
-            else:
-                return int(power)
-        else:
-            return {'error':'no value'}, 500
+            return int(power)
+        return {'error':'no value'}, 500
 
 class total_apparent_power(Resource):
     def get(self):
         power = read_counter(794)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
+
+class total_kwh(Resource):
+    def get(self):
+        kwh = read_counter(856)
+        if kwh is not None:
+            return int(kwh)
+        return {'error':'no value'}, 500
+
 
 class phase1_apparent_power(Resource):
     def get(self):
         power = read_counter(50556)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase1_real_power(Resource):
     def get(self):
         power = read_counter(50544)
-        if power != None:
+        if power is not None:
             # Real Power must be converted
             if int(power) > 32768:
                 return int(power)-65535
-            else:
-                return int(power)
-        else:
-            return {'error':'no value'}, 500
+            return int(power)
+        return {'error':'no value'}, 500
 
 class phase1_reactive_power(Resource):
     def get(self):
         power = read_counter(50550)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase1_power_factor(Resource):
     def get(self):
         factor = read_counter(50562)
-        if factor != None:
+        if factor is not None:
             return int(factor)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase2_apparent_power(Resource):
     def get(self):
         power = read_counter(50558)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase2_real_power(Resource):
     def get(self):
         power = read_counter(50546)
-        if power != None:
+        if power is not None:
             # Real Power must be converted
             if int(power) > 32768:
                 return int(power)-65535
-            else:
-                return int(power)
-        else:
-            return {'error':'no value'}, 500
+            return int(power)
+        return {'error':'no value'}, 500
 
 class phase2_reactive_power(Resource):
     def get(self):
         power = read_counter(50550)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase2_power_factor(Resource):
     def get(self):
         factor = read_counter(50564)
-        if factor != None:
+        if factor is not None:
             return int(factor)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase3_apparent_power(Resource):
     def get(self):
         power = read_counter(50560)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase3_real_power(Resource):
     def get(self):
         power = read_counter(50548)
-        if power != None:
+        if power is not None:
             # Real Power must be converted
             if int(power) > 32768:
                 return int(power)-65535
-            else:
-                return int(power)
-        else:
-            return {'error':'no value'}, 500
+            return int(power)
+        return {'error':'no value'}, 500
 
 class phase3_reactive_power(Resource):
     def get(self):
         power = read_counter(50550)
-        if power != None:
+        if power is not None:
             return int(power)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
 
 class phase3_power_factor(Resource):
     def get(self):
         factor = read_counter(50566)
-        if factor != None:
+        if factor is not None:
             return int(factor)
-        else:
-            return {'error':'no value'}, 500
+        return {'error':'no value'}, 500
